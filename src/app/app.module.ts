4652f4e0af0ded1e0df2f17a265b5808107f63d9
Pedro Santiago <santiagvs@proton.me>
2023-10-02 20:22:18 -0300

    criando componente para main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, MainPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
