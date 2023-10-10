504b6054bad4dcd5bbf9bbc8dad4b1a99b7064cb
Pedro Santiago <santiagvs@proton.me>
2023-10-10 11:07:17 -0300

    criando componente da seção de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { EscolaColetivaComponent } from './components/escola-coletiva/escola-coletiva.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent, MainPageComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, MainPageComponent, EscolaColetivaComponent, CursosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
