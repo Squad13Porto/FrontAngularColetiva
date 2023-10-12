eb2cf7d653d20f3e91929b3068a337a460d34887
Santiago <victor.lacsilva0102@gmail.com>
2023-10-12 20:11:18 -0300

    alterando responsividade, adicionando mensagens dinâmicas no button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
-
+  @Input('message') message!: string;
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,21 +3,27 @@ import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   isScrolled = false;
   logoSrc = '../../../assets/images/LOGOTIPO_HORIZONTAL_BRANCO.png';
-  scrolledLogoSrc = '../../../assets/images/LOGOTIPO_HORIZONTAL.png'
+  scrolledLogoSrc = '../../../assets/images/LOGOTIPO_HORIZONTAL.png';
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollPosition =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
     this.isScrolled = scrollPosition > 100;
     this.updateLogoSrc();
   }
 
   private updateLogoSrc(): void {
-    this.logoSrc = this.isScrolled ? this.scrolledLogoSrc : '../../../assets/images/LOGOTIPO_HORIZONTAL_BRANCO.png';
+    this.logoSrc = this.isScrolled
+      ? this.scrolledLogoSrc
+      : '../../../assets/images/LOGOTIPO_HORIZONTAL_BRANCO.png';
   }
 }
