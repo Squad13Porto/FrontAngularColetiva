bb351fab8462a3ef2fd5f45297cd3625fffaf807
Danillo <danillodana@outlook.com>
2023-10-17 10:40:58 -0300

    Criação do componente About-us e dos cards de colaboradoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { EscolaColetivaComponent } from './components/escola-coletiva/escola-coletiva.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { AulasComponent } from './components/aulas/aulas.component';
+import { CardCollaboratorComponent } from './components/card-collaborator/card-collaborator.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent, MainPageComponent, EscolaColetivaComponent, CursosComponent, AulasComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, MainPageComponent, EscolaColetivaComponent, CursosComponent, AulasComponent, CardCollaboratorComponent, AboutUsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/card-collaborator/card-collaborator.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-collaborator',
+  templateUrl: './card-collaborator.component.html',
+  styleUrls: ['./card-collaborator.component.css']
+})
+export class CardCollaboratorComponent {
+  @Input('collaboratorName') collaboratorName!: string;  
+  @Input('description') description!: string;
+}
