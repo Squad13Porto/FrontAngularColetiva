78b07e32d81641ebfd8ecefc3d0d3e9754ca4df2
nilloferreiira <danillodana@outlook.com>
2023-11-23 01:38:10 -0300

    passando src slide de aulas para o carrossel-ngx

--- src/app/components/carrossel-ngx/carrossel-ngx.component.ts
+++ src/app/components/carrossel-ngx/carrossel-ngx.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import $ from 'jquery';
+import { HttpClient } from '@angular/common/http';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
+import { Slide } from './slide';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-carrossel-ngx',
@@ -11,42 +12,47 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
   imports: [CommonModule, SlickCarouselModule],
   styleUrls: ['./carrossel-ngx.component.css'],
 })
-export class CarrosselNgxComponent{
-  // @Input('srcs') srcs!: {};
-  @Input('arrows') arrows!: boolean;
-  
-  isArrowsActive: boolean = this.arrows
+export class CarrosselNgxComponent {
+  //src imgs slide
+  @Input() slidesAulasAbertas: Slide[] = [];
+  @Input() slidesAulasPilulas: Slide[] = [];
 
-  constructor() { }
-  
+  constructor(private http: HttpClient) {
+    this.slidesAberta = [];
+    this.slidesPilula = [];
+  }
+
   // imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
-  
-  
   // preciso receber de uma propriedade com o ID do curso especifico, para colocar no <a>
   // dentro do html desse slide, e assim referencialo
   // <a routerLink="/first-component" routerLinkActive="active" ariaCurrentWhenActive="page">First Component</a>
-  
 
-  slides = [
-    {src: '../../../assets/images/reuniao.jpeg'},
-    {src: '../../../assets/images/img-aula-aberta.png'},
-    {src: '../../../assets/images/img-aula-pilula.png'},
-  ]
-  
-    slickConfig = {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      dots: true,
-      infinite: true,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: this.arrows //ver pq a variavel n ta funcionando
-    };
-  
-  
+  slidesAberta: Slide[];
+  slidesPilula: Slide[];
+
+  slickConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  // slidesAberta = [
+  //   {src: ''}
+  // ]
+
+  // slidesPilula = [
+  //   {src: ''}
+  // ]
+
   ngOnInit(): void {
+    this.slidesAberta = this.slidesAulasAbertas.map((item: any) => ({
+      src: item.src,
+    }));
+    this.slidesPilula = this.slidesAulasPilulas.map((item: any) => ({
+      src: item.src,
+    }));
   }
-
-
 }
-
--- src/app/components/landingPage/aulas/aulas.component.ts
+++ src/app/components/landingPage/aulas/aulas.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-aulas',
   templateUrl: './aulas.component.html',
   styleUrls: ['./aulas.component.css']
 })
+
+
 export class AulasComponent {
-
-  slideImgs: any;
-
+  aulasAbertas: any;
+  aulasPilulas: any;
+  
+  
   constructor(private http: HttpClient) {  }
+  slidesAberta = [
+    {src: '../../../assets/images/reuniao.jpeg'},
+    {src: '../../../assets/images/img-aula-aberta.png'},
+    {src: '../../../assets/images/img-aula-pilula.png'},
+    // {src: ''}
+  ]
 
+  slidesPilula = [
+    {src: '../../../assets/images/reuniao.jpeg'},
+    {src: '../../../assets/images/img-aula-aberta.png'},
+    {src: '../../../assets/images/img-aula-pilula.png'},
+    // {src: ''}
+  ]
+
+  
   ngOnInit(): void {
-    this.http.get("http://177.153.50.192:5001/squad13-7ae1e/us-central1/getFirestoreDocs")
-      .subscribe((response: any) => {
-        this.slideImgs = response.aulasAbertasUrls;
-        console.log(typeof(this.slideImgs)) // retrona object
-      });      
-  }
+    this.http.get("https://us-central1-squad13-7ae1e.cloudfunctions.net/getFirestoreDocs")
+    .subscribe((response: any) => {
+      this.aulasAbertas = response.aulasAbertasUrls;  // retrona object
+      this.aulasPilulas = response.aulasPilulasUrls;
+      
+      
+      // this.slidesAberta = [...this.aulasAbertas.map((item: any) => ({
+      //   src: item 
+      // }))];
+
+      // this.slidesPilula = [...this.aulasPilulas.map((item: any) => ({
+      //   src: item 
+      // }))];
+
+      // console.log(this.slidesAberta)
+      // console.log('////////////////')
+      // console.log(this.slidesPilula)
+
+
+    });      
 }
+ }
