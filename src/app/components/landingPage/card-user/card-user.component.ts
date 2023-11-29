01b3fa38a094e25538611f5299fecd81abdbc321
Santiago <victor.lacsilva0102@gmail.com>
2023-11-29 17:03:23 -0300

    feat: adicionando os cards dinâmicos para os usuários

--- src/app/components/landingPage/card-user/card-user.component.ts
+++ src/app/components/landingPage/card-user/card-user.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card-user',
   templateUrl: './card-user.component.html',
-  styleUrls: ['./card-user.component.css']
+  styleUrls: ['./card-user.component.css'],
 })
 export class CardUserComponent {
+  cards: any;
 
+  constructor(private http: HttpClient) {
+    this.cards = [];
+  }
+
+  ngOnInit(): void {
+    this.http
+      .get(
+        'https://us-central1-squad13-7ae1e.cloudfunctions.net/getFirestoreDocs'
+      )
+      .subscribe((response: any) => {
+        this.cards = response.cardsValidacao;
+      });
+  }
 }
